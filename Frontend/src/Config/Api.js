effbbd74d17f345f91631817803618dc83545461
Akhi <97934892+Akhilmak@users.noreply.github.com>
2024-11-01 22:33:34 +0530

    Updated baseUrl to live backend

--- Frontend/src/Config/Api.js
+++ Frontend/src/Config/Api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:8080";
+// export const baseUrl = "http://localhost:8080"; //use this when deploying inn localmachine
+
+export const baseUrl = "https://music-m5on.onrender.com";
 
 const api=axios.create({
     baseURL:baseUrl,
